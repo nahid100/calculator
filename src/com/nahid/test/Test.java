e2142f1a8c5242e826efd67487c4ea65c7436aae
nahid sanwar <nsanwar2014@gmail.com>
2019-07-07 16:17:45 -0400

    initial commit

---
+++ src/com/nahid/test/Test.java
@@ -0,0 +1,16 @@
+package com.nahid.test;
+
+public class Test {
+	
+	private static String name;
+
+	public static void main(String[] args) {
+		
+		name = "Nahid";
+		
+		System.out.println(name);
+		
+		
+	}
+
+}
